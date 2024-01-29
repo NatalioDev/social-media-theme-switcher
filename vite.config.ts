47cae6b05aee6048a3a879b2e73abd72b1ff7da1
NatalioDev <nataliodg.dev@gmail.com>
2024-01-29 10:19:10 -0300

    deploy gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import typescript from "@rollup/plugin-typescript"
 
 // https://vitejs.dev/config/
 export  default  defineConfig ( ( { command } ) =>  { 
   const  config = { 
-    plugins : [ react ()], 
+    plugins : [ react (), typescript()], 
     base :  '/' , 
   } 
 
