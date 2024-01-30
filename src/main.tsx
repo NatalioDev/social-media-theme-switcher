90c72e90b4b6413a5e0d9f75c448a7739cac607a
NatalioDev <nataliodg.dev@gmail.com>
2024-01-30 20:17:05 -0300

    update styles

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeContextProvider>
     <React.StrictMode>
       <App/>
-    </React.StrictMode>,
+    </React.StrictMode>
   </ThemeContextProvider>
 
 )
